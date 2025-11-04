c86c97babe7e47e7748197e88dcaae930c67f207
ran130987wan <ran13wan@gmail.com>
2025-11-04 19:23:58 +0530

    Add commented output variables for ACR
    Add commented output variables for ACR configuration.

---
+++ platform/terraform/global/output.tf
@@ -0,0 +1,7 @@
+# output "acr_name" {
+#   value = module.vdc_entra_acr.name
+# }
+
+# output "acr_resource_group" {
+#   value = "${var.global_config.prefix}-global-container-rg-${var.global_config.environment}"
+# }
