b5e31a6c0acde1345b6ec8c9fb25800d885daac5
ran13wan <ran13wan@gmail.com>
2025-11-04 19:58:25 +0530

    update

--- platform/terraform/global/locals.tf
+++ platform/terraform/global/locals.tf
@@ -35,7 +35,7 @@ locals {
   }
   region_code                = var.azure_region_map[var.global_config.location]
   enable_resource_group_lock = var.global_config.environment == "prod" || var.global_config.environment == "stage" ? true : false
-  
+
   resource_groups = [
     for rg in local.resource_groups_map : {
       name     = "${var.global_config.compact_prefix}-${rg.name}-rg-${var.global_config.environment}"
