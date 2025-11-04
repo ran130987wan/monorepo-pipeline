80625acbfeb5d27c13c38249aaf754e0b3f665e7
ran13wan <ran13wan@gmail.com>
2025-11-04 20:01:17 +0530

    update

--- platform/terraform/modules/federated-identity-credential/main.tf
+++ platform/terraform/modules/federated-identity-credential/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_federated_identity_credential" "federated_identity" {
-  name                = var.federated_identity_credential.name
-  resource_group_name = var.federated_identity_credential.resource_group_name
-  parent_id           = var.federated_identity_credential.parent_id
-  audience            = var.federated_identity_credential.audience
-  issuer              = var.federated_identity_credential.issuer
-  subject             = var.federated_identity_credential.subject
+  name                = var.federated_identity_credential.name                # Name of the federated identity credential
+  resource_group_name = var.federated_identity_credential.resource_group_name # Resource group containing the managed identity
+  parent_id           = var.federated_identity_credential.parent_id           # ID of the parent managed identity
+  audience            = var.federated_identity_credential.audience            # List of audiences that can appear in the external token
+  issuer              = var.federated_identity_credential.issuer              # URL of the external identity provider (e.g., GitHub Actions)
+  subject             = var.federated_identity_credential.subject             # Subject claim from the external token to match
 }
