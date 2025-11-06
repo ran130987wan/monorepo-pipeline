ffbacc704c27296091544db484dfac4c65de0289
ran130987wan <ran13wan@gmail.com>
2025-11-06 02:04:51 +0000

    update

--- platform/terraform/modules/managed-identity/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_user_assigned_identity" "umi" {           # Create Azure User Assigned Managed Identity
-  name                = var.managed_identity.umi_name       # Name of the managed identity
-  location            = var.managed_identity.location       # Azure region for the identity
-  resource_group_name = var.managed_identity.resource_group_name # Resource group containing the identity
-  tags                = var.managed_identity.tags           # Tags for resource organization
-}
